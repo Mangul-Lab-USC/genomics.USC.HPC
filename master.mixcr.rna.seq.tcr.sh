338b82e4e91e4c4b8a9ac348678f4dcbec23f7e6
Aaron Karlsberg <akarlsbe@hpc-login3.hpcc.usc.edu>
2019-11-01 22:53:19 -0700

    fixed bug

--- master.mixcr.rna.seq.tcr.sh
+++ master.mixcr.rna.seq.tcr.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}'>samples.txt
 
 
@@ -8,14 +9,14 @@ do
 pwd=$(pwd)
 
 
+echo "#!/bin/sh" > run.mixcr.rna_seq.${line}.sh
 
-echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 align -p rna-seq -s hsa -OallowPartialAlignments=true ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}_alignments.vdjca" > run.mixcr.rna_seq.${line}.sh
+echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 align -p rna-seq -s hsa -OallowPartialAlignments=true ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}_alignments.vdjca" >> run.mixcr.rna_seq.${line}.sh
 echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 assemblePartial ${line}_alignments.vdjca ${line}_alignments_rescued_1.vdjca" >> run.mixcr.rna_seq.${line}.sh
 echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 assemblePartial ${line}_alignments_rescued_1.vdjca ${line}_alignments_rescued_2.vdjca" >> run.mixcr.rna_seq.${line}.sh
 echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 assemble ${line}_alignments_rescued_2.vdjca ${line}_clones.clns" >> run.mixcr.rna_seq.${line}.sh
 echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 exportClones ${line}_clones.clns ${line}_clones.txt" >> run.mixcr.rna_seq.${line}.sh
 
-
-sbatch run.mixcr.rna_seq.${line}.sh
+sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.mixcr.rna_seq.${line}.sh
 
 done<samples.txt
