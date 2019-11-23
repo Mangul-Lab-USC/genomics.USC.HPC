5b8c971660fd298b02d26327806cd482942f83ce
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-11-22 18:22:18 -0800

    bug

--- master.hisat2.tuned.SE.sh
+++ master.hisat2.tuned.SE.sh
@@ -7,7 +7,7 @@ while read line
 do
 
 echo "#!/bin/sh" > run.${line}.sh
-echo "/home/rcf-proj/sm3/akarlsbe/genomics.USC.HPC/run.hisat2.tuned.SE.sh ${line}_R2_001.fastq.gz ${line}">run.${line}.sh
+echo "/home/rcf-proj/sm3/akarlsbe/genomics.USC.HPC/run.hisat2.tuned.SE.sh ${line}_R2_001.fastq.gz ${line}">>run.${line}.sh
 sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.${line}.sh
 
 done<samples.txt
