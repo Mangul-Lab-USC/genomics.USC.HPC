a493333dbc3f9be1dcfe0dfd357f7dce126bedfc
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-11-25 17:32:08 -0800

    filepath

--- master.imrep.bam.hg38.sh
+++ master.imrep.bam.hg38.sh
@@ -8,7 +8,7 @@ do
 
 # for star.leafcutter prepped bams, use option: --chrFormat2 
 echo "#!/bin/sh" > run.imrep.${line}.sh
-echo "/home/rcf-proj/sm3/akarlsbe/anaconda2/bin/python /u/home/s/serghei/project/code/seeing.beyond.target/tools/imrep/imrep.py --hg38 --bam  --noOverlapStep --noCast ${line}.bam ${line}.cdr3" >>run.imrep.${line}.sh
+echo "/home/rcf-proj/sm3/akarlsbe/anaconda2/bin/python /home/rcf-proj/sm3/akarlsbe/code/imrep/imrep.py --hg38 --bam  --noOverlapStep --noCast ${line}.bam ${line}.cdr3" >>run.imrep.${line}.sh
 echo "ls>done.txt" >> run.imrep.${line}.sh
 
 sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.imrep.${line}.sh -p scavenge
