accfb6ea968b4ba8cf6c70adbd81794bbae2bf4b
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-10 10:20:38 -0800

    pe

--- master.hisat2.tuned.PE.sh
+++ master.hisat2.tuned.PE.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-ls *_R2_001.fastq.gz | awk -F "_R2_001.fastq.gz" '{print $1}'>samples.txt
+ls *_R1_001.fastq.gz | awk -F "_R1_001.fastq.gz" '{print $1}'>samples.txt
 
 
 while read line
 do
 
 echo "#!/bin/sh" > run.hisat2.${line}.sh
-echo "/home/rcf-proj/sm3/akarlsbe/genomics.USC.HPC/run.hisat2.tuned.SE.sh ${line}_R2_001.fastq.gz ${line}">>run.hisat2.${line}.sh
+echo "/home/rcf-proj/sm3/akarlsbe/genomics.USC.HPC/run.hisat2.tuned.PE.sh ${line}_R1_001.fastq.gz ${line}_R2_001.fastq.gz ${line}">>run.hisat2.${line}.sh
 echo "ls>done.txt" >> run.hisat2.${line}.sh
 
-sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.hisat2.${line}.sh -p scavenge
+sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.hisat2.${line}.sh #-p scavenge
 
 done<samples.txt
+
+
+
+
+
