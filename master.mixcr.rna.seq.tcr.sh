bf7c4345bba7aa93d8745aac8fbf968cfcdde864
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-11 13:23:43 -0800

    fixed mixcr path

--- master.mixcr.rna.seq.tcr.sh
+++ master.mixcr.rna.seq.tcr.sh
@@ -11,11 +11,11 @@ pwd=$(pwd)
 
 echo "#!/bin/sh" > run.mixcr.rna_seq.${line}.sh
 
-echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 align -p rna-seq -s hsa -OallowPartialAlignments=true ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}_alignments.vdjca" >> run.mixcr.rna_seq.${line}.sh
-echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 assemblePartial ${line}_alignments.vdjca ${line}_alignments_rescued_1.vdjca" >> run.mixcr.rna_seq.${line}.sh
-echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 assemblePartial ${line}_alignments_rescued_1.vdjca ${line}_alignments_rescued_2.vdjca" >> run.mixcr.rna_seq.${line}.sh
-echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 assemble ${line}_alignments_rescued_2.vdjca ${line}_clones.clns" >> run.mixcr.rna_seq.${line}.sh
-echo "java -jar /home/rcf-proj3/sm3/akarlsbe/mixcr-3.0.11 exportClones ${line}_clones.clns ${line}_clones.txt" >> run.mixcr.rna_seq.${line}.sh
+echo "java -jar /home/rcf-proj/sm3/akarlsbe/code/mixcr-3.0.11 align -p rna-seq -s hsa -OallowPartialAlignments=true ${pwd}/${line}_R1_001.fastq.gz ${pwd}/${line}_R2_001.fastq.gz ${line}_alignments.vdjca" >> run.mixcr.rna_seq.${line}.sh
+echo "java -jar /home/rcf-proj/sm3/akarlsbe/code/mixcr-3.0.11 assemblePartial ${line}_alignments.vdjca ${line}_alignments_rescued_1.vdjca" >> run.mixcr.rna_seq.${line}.sh
+echo "java -jar /home/rcf-proj/sm3/akarlsbe/code/mixcr-3.0.11 assemblePartial ${line}_alignments_rescued_1.vdjca ${line}_alignments_rescued_2.vdjca" >> run.mixcr.rna_seq.${line}.sh
+echo "java -jar /home/rcf-proj/sm3/akarlsbe/code/mixcr-3.0.11 assemble ${line}_alignments_rescued_2.vdjca ${line}_clones.clns" >> run.mixcr.rna_seq.${line}.sh
+echo "java -jar /home/rcf-proj/sm3/akarlsbe/code/mixcr-3.0.11 exportClones ${line}_clones.clns ${line}_clones.txt" >> run.mixcr.rna_seq.${line}.sh
 echo "ls>done.txt" >> run.mixcr.rna_seq.${line}.sh
 
 sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.mixcr.rna_seq.${line}.sh #-p scavenge
