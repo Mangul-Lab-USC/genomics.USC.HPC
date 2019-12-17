eb4a748a536dc9a69a70b2abce5fd3678c3cdfc5
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-16 16:47:44 -0800

    clonality instructions

--- master.clonality.sh
+++ master.clonality.sh
@@ -16,7 +16,15 @@ done<samples.txt
 
 
 
-#command to combine the summary.cdr3.txt files into one csv for all clonality samples.
+#command to combine the TCRA.cdr3.FREQ..csv files into one csv for all clonality samples.
 #echo "SAMPLE,nIGH,nIGK,nIGL,nTCRA,nTCRB,nTCRD,nTCRG,loadIGH,loadIGK,loadIGL,loadTCRA,loadTCRB,loadTCRD,loadTCRG,alphaIGH,alphaIGK,alphaIGL,alphaTCRA,alphaTCRB,alphaTCRD,alphaTCRG" > combined_clonality.csv
 #tail -n +2 -q */*summary.cdr3.txt >> combined_clonality.csv
 
+
+#command to combine the summary.cdr3.txt files into one csv for all clonality samples.
+grep "" */TCRA.cdr3.FREQ..csv | sed 's/:/,/' | awk -F "_" '{print $2"_"$3}' >t1
+grep "" */TCRA.cdr3.FREQ..csv | sed 's/:/,/' | awk -F "," '{print $2","$3","$4}' >t2
+paste t1 t2 | awk '{print $1","$2}' > t3
+grep -v CDR3 t3 > t4
+echo "SAMPLE,CDR3,count,relative.frequency" > t5 
+cat t5 t4 > combined_cdr3_frequencies.csv
