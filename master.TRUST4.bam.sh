e492d9631a878645acf722ca1679154be6fdba59
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-21 17:22:24 -0800

    trust4

--- master.TRUST4.bam.sh
+++ master.TRUST4.bam.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-source /usr/usc/gnu/gcc/8.2.0/setup.sh
-export LD_LIBRARY_PATH=/usr/usc/gnu/gcc/8.2.0/lib64:${LD_LIBRARY_PATH}
+ls *bam | awk -F ".bam" '{print $1}' >samples.txt
+
+while read line
+do
+
+
+echo "#!/bin/sh" > run.TRUST4.${line}.sh
+echo "source /usr/usc/gnu/gcc/8.2.0/setup.sh" >>run.TRUST4.${line}.sh
+echo "export LD_LIBRARY_PATH=/usr/usc/gnu/gcc/8.2.0/lib64:${LD_LIBRARY_PATH}" >>run.TRUST4.${line}.sh
+echo "mkdir TRUST4.${line}" >> run.TRUST4.${line}.sh
+echo "./run-trust4 -b ${line}.bam -f hg38_bcrtcr.fa --ref human_IMGT+C.fa -o TRUST4.${line}/${line}." >>run.TRUST4.${line}.sh
+
+echo "ls>done.txt" >> run.TRUST4.${line}.sh
+
+sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.TRUST4.${line}.sh #-p scavenge
+
+done<samples.txt
+
--- master.trust.bam.sh
+++ master.trust.bam.sh
@@ -2,10 +2,12 @@
 
 
 
-/home/rcf-proj/sm3/akarlsbe/anaconda2/bin/python2.7 
+/home/rcf-proj/sm3/akarlsbe/code/trust/bin/trust -f ${line}.bam -g hg38 -B -L
 
 
 
 # TRUST INSTALLATION NOTES USC HPC
 
 # Depends on python version 2.7
+# /home/rcf-proj/sm3/akarlsbe/anaconda2/bin/python2.7 
+
