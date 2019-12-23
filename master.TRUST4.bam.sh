aa06ab53c09593e6eae8c401b2fce1deedc10d12
aaronkarlsberg <aaron.karlsberg@gmail.com>
2019-12-23 11:36:13 -0800

    trust4 bug

--- master.TRUST4.bam.sh
+++ master.TRUST4.bam.sh
@@ -10,11 +10,10 @@ echo "#!/bin/sh" > run.TRUST4.${line}.sh
 echo "source /usr/usc/gnu/gcc/8.2.0/setup.sh" >>run.TRUST4.${line}.sh
 echo "export LD_LIBRARY_PATH=/usr/usc/gnu/gcc/8.2.0/lib64:${LD_LIBRARY_PATH}" >>run.TRUST4.${line}.sh
 echo "mkdir TRUST4.${line}" >> run.TRUST4.${line}.sh
-echo "/home/rcf-proj/sm3/akarlsbe/code/TRUST4/run-trust4 -b ${line}.bam -f hg38_bcrtcr.fa --ref human_IMGT+C.fa -o TRUST4.${line}/${line}." >>run.TRUST4.${line}.sh
+echo "/home/rcf-proj/sm3/akarlsbe/code/TRUST4/run-trust4 -b ${line}.bam -f /home/rcf-proj/sm3/akarlsbe/code/TRUST4/hg38_bcrtcr.fa --ref /home/rcf-proj/sm3/akarlsbe/code/TRUST4/human_IMGT+C.fa -o TRUST4.${line}/${line}." >>run.TRUST4.${line}.sh
 
 echo "ls>done.txt" >> run.TRUST4.${line}.sh
 
-sbatch --ntasks=16 --mem-per-cpu=16G --time=24:00:00 run.TRUST4.${line}.sh #-p scavenge
+sbatch --ntasks=4 --mem-per-cpu=16G --time=24:00:00 run.TRUST4.${line}.sh #-p scavenge
 
 done<samples.txt
-
